d41d126cd520e6c10dd30b37bc58bfdeb34168e3
jakob <jakob@winkk.com>
2026-07-24 19:36:11 +0200

    Update cask to 1.0.22

--- Casks/codex-profile-switcher.rb
+++ Casks/codex-profile-switcher.rb
@@ -1,6 +1,6 @@
 cask "codex-profile-switcher" do
-  version "1.0.21"
-  sha256 "a08bd23b84a64efbaaa009c008e3b0f19d7369f22b74aaf8c7bf45794bbe8373"
+  version "1.0.22"
+  sha256 "34f39d8f96a07b81197fb732502b640a5e54f209d8c33205465e47b9e8b1f9fa"
 
   url "https://github.com/JakobStadlhuber/Codex-Profile-Switcher/releases/download/v#{version}/Codex-Profiles-#{version}.zip"
   name "Codex Profiles"
